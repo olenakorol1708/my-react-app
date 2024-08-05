ab61f2c31813b6200a7c7d73d7b229884ff2189c
olenakorol1708 <olenakorol1708@gmail.com>
2024-08-05 10:27:23 +0300

    project initialization

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.scss'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <div className="card">
+      
+        
+      </div>
+     
+    </>
+  )
+}
+
+export default App
