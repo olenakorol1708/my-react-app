a26ea8df81c8528813eb21f0be88f8ab84bf67c5
olenakorol1708 <olenakorol1708@gmail.com>
2024-12-04 13:35:01 +0200

    Added a block with an algorithm of work

---
+++ src/Components/GetStartedButton/GetStarted.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'antd';
+import './style.scss';
+
+const GetStarted: React.FC = () => {
+  return (
+    <div className='works-button'>
+      <Button className="works-get-started">Get started</Button>
+    </div>
+  );
+};
+
+export default GetStarted;
