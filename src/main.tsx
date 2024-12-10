7b56176a022b5497e31a754462f727b1b7265311
olenakorol1708 <olenakorol1708@gmail.com>
2024-12-10 15:29:50 +0200

    Виправлено src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
+import { router } from './Routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
