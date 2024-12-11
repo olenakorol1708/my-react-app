591a84c392678e9c2fb0f5a661d414ecce19c4e7
olenakorol1708 <olenakorol1708@gmail.com>
2024-12-11 14:27:04 +0200

    fixed mistakes in style

--- src/Components/ReviewSlider/ReviewSlider.tsx
+++ src/Components/ReviewSlider/ReviewSlider.tsx
@@ -13,6 +13,7 @@ const ReviewSlider: React.FC = () => {
   }
 
   const review = reviews[index];
+  const nextReview = reviews[(index + 1) % reviews.length];
 
   const handleNextClick = () => {
     setIndex(prevIndex => (prevIndex + 1) % reviews.length);
@@ -41,30 +42,71 @@ const ReviewSlider: React.FC = () => {
             </Button>
           </div>
         </div>
-        <div className="clients-card">
-          <div className="clients-rate">
-            <div className="rate-number">{review.rate.toFixed(1)}</div>
-            <div className="rate-stars">
-              {Array.from({ length: Math.min(review.rate, 5) }).map((_, i) => (
-                <img key={i} src={review.starUrl} alt="star" className="star" />
-              ))}
+        <div className="clients-cards-container">
+          <div className="clients-card current ">
+            <div className="clients-rate-block">
+              <div className="rate-number">{review.rate.toFixed(1)}</div>
+              <div className="rate-stars">
+                {Array.from({ length: Math.min(review.rate, 5) }).map(
+                  (_, i) => (
+                    <img
+                      key={i}
+                      src={review.starUrl}
+                      alt="star"
+                      className="star"
+                    />
+                  )
+                )}
+              </div>
+            </div>
+            <div className="review-underline"></div>
+            <p className="review-text">{review.text}</p>
+            <div className="profile">
+              <div className="profile-img">
+                <img
+                  src={review.imageUrl}
+                  alt={`${review.personName}'s profile`}
+                />
+              </div>
+              <div className="profile-info">
+                <span className="review-name">{review.personName}</span>
+                <span className="review-title">{review.personTitle}</span>
+              </div>
             </div>
           </div>
-          <div className="underline"></div>
-          <p className="review-text">{review.text}</p>
-          <div className="profile">
-            <div className="profile-img">
-              <img
-                src={review.imageUrl}
-                alt={`${review.personName}'s profile`}
-              />
+          <div className="clients-card next">
+            <div className="clients-rate-block">
+              <div className="rate-number">{nextReview.rate.toFixed(1)}</div>
+              <div className="rate-stars">
+                {Array.from({ length: Math.min(nextReview.rate, 5) }).map(
+                  (_, i) => (
+                    <img
+                      key={i}
+                      src={nextReview.starUrl}
+                      alt="star"
+                      className="star"
+                    />
+                  )
+                )}
+              </div>
             </div>
-            <div className="profile-info">
-              <span className="review-name">{review.personName}</span>
-              <span className="review-title">{review.personTitle}</span>
+            <div className="review-underline"></div>
+            <p className="review-text">{nextReview.text}</p>
+            <div className="profile">
+              <div className="profile-img">
+                <img
+                  src={nextReview.imageUrl}
+                  alt={`${nextReview.personName}'s profile`}
+                />
+              </div>
+              <div className="profile-info">
+                <span className="review-name">{nextReview.personName}</span>
+                <span className="review-title">{nextReview.personTitle}</span>
+              </div>
             </div>
           </div>
         </div>
+
         <div className="pagination">
           {reviews.map((_, i) => (
             <span
